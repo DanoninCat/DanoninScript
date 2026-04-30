b01778b3b10af1182404c7da6216dadb31153577
DanoninCat <mickeydragon412@gmail.com>
2026-04-30 01:14:14 -0300

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -3,7 +3,7 @@
 --  Desenvolvido por Danonin
 -- ============================================================
 
-local URL = "https://raw.githubusercontent.com/DanoninCat/DanoninScript/refs/heads/main/Scripts/CatEmpire_v5_8.lua"
+local URL = "https://raw.githubusercontent.com/DanoninCat/DanoninScript/refs/heads/main/Scripts/CatEmpire_v5_10.lua"
 
 local ok, err = pcall(function()
     loadstring(game:HttpGet(URL, true))()
