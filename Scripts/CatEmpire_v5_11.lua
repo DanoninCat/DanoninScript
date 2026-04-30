63849404e0869b718f418513747f7982adf430a2
DanoninCat <mickeydragon412@gmail.com>
2026-04-30 02:40:53 -0300

    Update CatEmpire_v5_11.lua

--- Scripts/CatEmpire_v5_11.lua
+++ Scripts/CatEmpire_v5_11.lua
@@ -1,7 +1,5 @@
 -- ============================================================
---  CAT EMPIRE | v5.8 | CODED FOR DANONIN
---  UI: Fluent Library
---  Fixes: getNearestEnemy usa atributo ID real, Fighter scan usa Name como UID, mundos corretos
+--  CAT EMPIRE | v5.11 | CODED FOR DANONIN
 -- ============================================================
 
 -- SERVICES
