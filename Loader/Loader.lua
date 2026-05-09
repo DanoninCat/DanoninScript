8ec774ce638f98d775efaa43afac01d0c0ffb05d
DanoninCat <mickeydragon412@gmail.com>
2026-05-09 12:15:51 -0300

    Delete Loader/Loader.lua

--- Loader/Loader.lua
+++
@@ -1,6 +0,0 @@
--- ============================================================
---  CAT EMPIRE | LOADER | Desenvolvido por Danonin
--- ============================================================
-local URL = "https://raw.githubusercontent.com/DanoninCat/DanoninScript/refs/heads/main/Scripts/CatEmpire_v5_19.lua"
-local ok, err = pcall(function() loadstring(game:HttpGet(URL, true))() end)
-if not ok then warn("[CatEmpire] Erro: " .. tostring(err)) end
