faa3704f6603bc0b341b82770e79766ebf79716f
DanoninCat <mickeydragon412@gmail.com>
2026-08-08 12:31:01 -0300

    Update Loader.lua

--- Loader/Loader.lua
+++ Loader/Loader.lua
@@ -9,6 +9,7 @@ local Games = {
     [18923620224]    = BASE .. "AnimeWarriors3.lua",
     [75995379831247] = BASE .. "DigAndGrow.lua",
     [72875403687172] = BASE .. "AnimeOverdrive.lua",
+    [94717504417144] = BASE .. "AnimeCapture.lua",
 }
 local script = Games[game.PlaceId]
 if not script then
