d0221ec18f74fe338800c2aa545a3aa251f07135
DanoninCat <mickeydragon412@gmail.com>
2026-09-22 17:43:23 -0300

    feat: add macro defaults

---
+++ ReAdventuresV2/Macros/Config.lua
@@ -0,0 +1,18 @@
+-- Re Adventures V2 - Macros defaults
+
+return {
+    recorder = {
+        snapshotInterval = 1,
+        maxSnapshots = 7200,
+    },
+
+    library = {
+        autoSelectNewest = true,
+        autoSaveStoppedRecording = true,
+    },
+
+    export = {
+        schema = "re-adventures-macro",
+        version = 2,
+    },
+}
